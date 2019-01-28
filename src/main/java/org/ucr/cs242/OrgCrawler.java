7929a21b1978da6aae3cdedd510dee682a002ecc
Sunita Koppar <sunitakoppar@sunitas-mbp.lan>
2019-01-27 23:09:11 -0800

    Initial Crawler version

---
+++ src/main/java/org/ucr/cs242/OrgCrawler.java
@@ -0,0 +1,45 @@
+package org.ucr.cs242;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class  OrgCrawler {
+    private static final int MAX_DEPTH = 10;
+    private HashSet<String> links;
+
+    public OrgCrawler() {
+        links = new HashSet();
+    }
+
+    public void getPageLinks(String URL, int depth) {
+        if ((!links.contains(URL) && (depth < MAX_DEPTH))) {
+            System.out.println(">> Depth: " + depth + " [" + URL + "]");
+            try {
+                links.add(URL);
+
+                Document document = Jsoup.connect(URL).get();
+                Elements linksOnPage = document.select("a[href]");
+
+                depth++;
+                for (Element page : linksOnPage) {
+                    getPageLinks(page.attr("abs:href"), depth);
+                }
+            } catch (IOException e) {
+                System.err.println("For '" + URL + "': " + e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new OrgCrawler().getPageLinks("https://www.wango.org/", 0);
+    }
+}
